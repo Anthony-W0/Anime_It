7edb0ab6172dd7cead72da34780c8d2dd4ab130d
Zephina <anthonywhitmore2@gmail.com>
2026-04-01 17:08:36 -0500

    Update sample user data in Usersampler.swift

--- Anime,it/Anime,it/Usersampler.swift
+++ Anime,it/Anime,it/Usersampler.swift
@@ -8,15 +8,34 @@
 import SwiftUI
 import UIKit
 
-let samplerUser = User(
-    id: "1",
-    username: "testuser",
-    displayname : "User",
-    bio: "New user and new artist",
-    links: "Enter link here",
-    profileImageURL: "",
+//
+//  Sampler.swift
+//  Anime it
+//
+//  Created by Anthony Whitmore on 4/1/26.
+//
+
+import SwiftUI
+import UIKit
+
+#Preview {
+    let sampleUser = User(
+        id: "1",
+        username: "zephina",
+        displayname: "Zephina",
+        bio: "Anime artist 🎨",
+        links: "https://google.com",
+        profileImageName: "Profile", // asset name
+        artworkImageName: "Artwork_zephina",  // asset name
+        isFollowing: 23,
+        isFollower: 54,
+        artworkCount: 12
+    )
     
-    isFollowing: 23,
-    isFollower: 54,
-    artworkCount: 12
-)
+    return ProfileView(
+        user: sampleUser,
+        featuredImageNames: ["Zephina pics", "Zephina pic 2", "Zephina pic 3", "Zephina pic 4"] // assets
+    )
+}
+
+
