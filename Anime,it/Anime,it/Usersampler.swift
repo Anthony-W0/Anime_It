82a7a201bace1459079b40d9521382a3da445edb
Zephina <anthonywhitmore2@gmail.com>
2026-04-02 15:33:30 -0500

    Modify sample user data in Usersampler.swift
    Updated user bio and artwork count in sample user data.

--- Anime,it/Anime,it/Usersampler.swift
+++ Anime,it/Anime,it/Usersampler.swift
@@ -1,14 +1,4 @@
 //
-//  Usersampler.swift
-//  Anime,it
-//
-//  Created by Anthony Whitmore on 3/26/26.
-//
-
-import SwiftUI
-import UIKit
-
-//
 //  Sampler.swift
 //  Anime it
 //
@@ -23,19 +13,20 @@ import UIKit
         id: "1",
         username: "zephina",
         displayname: "Zephina",
-        bio: "Anime artist 🎨",
+        bio: "Doing art for fun :3",
         links: "https://google.com",
         profileImageName: "Profile", // asset name
         artworkImageName: "Artwork_zephina",  // asset name
         isFollowing: 23,
         isFollower: 54,
-        artworkCount: 12
+        artworkCount: 4
     )
     
     return ProfileView(
         user: sampleUser,
         featuredImageNames: ["Zephina pics", "Zephina pic 2", "Zephina pic 3", "Zephina pic 4"] // assets
     )
+    
 }
 
 
