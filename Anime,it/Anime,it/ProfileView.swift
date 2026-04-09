68dd0e95f13a13352af3f4c615649eb40292744e
Zephina <anthonywhitmore2@gmail.com>
2026-04-09 11:59:52 -0500

    Update ProfileView.swift

--- Anime,it/Anime,it/ProfileView.swift
+++ Anime,it/Anime,it/ProfileView.swift
@@ -1,17 +1,34 @@
 import SwiftUI
 
 struct ProfileView: View {
+   
    @StateObject var vm = UserViewModel()
+   
    var body: some View {
-       ScrollView {
-           VStack(spacing: 16) {
-               ProfileHeader()
-               ProfileStatsView()
+      NavigationView {
+          ScrollView {
+             VStack(spacing: 20) {
+                                  ForEach(vm.users) { user in
 
-               Text("User Posts Here")
-           }
-           .padding()
-       }
+                                                     Vstack(spacing:12) {
+                                                        ProfileHeader(user: user)
+                                                        ProfileStatsView(user: user)
+
+                                                        Divider()
+                                                     }
+                                                     .padding()
+                                                    }
+             }
+          }
+         .navigationTitle("Profiles")
+      }
+      .onAppear {
+         .vm.fecthUsers()
+      }
    }
 }
+#Preview {
+   ProfileView()
+}
+
 
