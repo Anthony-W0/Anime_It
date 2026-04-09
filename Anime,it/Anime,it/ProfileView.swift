9dae504895f203abaf554703b90cf3800e8f760a
Zephina <anthonywhitmore2@gmail.com>
2026-04-09 11:42:34 -0500

    Refactor ProfileView layout and components

--- Anime,it/Anime,it/ProfileView.swift
+++ Anime,it/Anime,it/ProfileView.swift
@@ -1,54 +1,16 @@
 import SwiftUI
 
 struct ProfileView: View {
-    var user: User
-    
-    // Featured image(s) UNDER bio
-    var featuredImageNames: [String] = []
-    
-    var body: some View {
-        ScrollView {
-            VStack(spacing: 24) {
-                
-                // MARK: - Header + Bio
-                VStack(alignment: .leading, spacing: 12) {
-                    
-                    // Profile header
-                    BioSectionView(user: user)
-                    
-                    
-                    
-                    // Edit Profile button
-                    Button("Edit Profile") 
-                    {
-                        // Action here
-                    }
-                    .frame(maxWidth: .infinity)
-                    .padding(8)
-                    .background(Color(hex: "#a5d4d9"))
-                    .cornerRadius(8)
-                    
-                    // Featured / bio images
-                    if !featuredImageNames.isEmpty {
-                        VStack(spacing: 16) {
-                            ForEach(featuredImageNames, id: \.self) { imageName in
-                                Image(imageName)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(height: 200)
-                                    .clipped()
-                                    .cornerRadius(12)
-                            }
-                        }
-                    }
-                }
-                .padding()
-                .background(Color(hex: "#c4f0f5"))
-                .cornerRadius(12)
-                .padding(.horizontal)
-            }
-            .padding(.top)
-        }
-    }
+   var body: some View {
+       ScrollView {
+           VStack(spacing: 16) {
+               ProfileHeader()
+               ProfileStatsView()
+
+               Text("User Posts Here")
+           }
+           .padding()
+       }
+   }
 }
 
